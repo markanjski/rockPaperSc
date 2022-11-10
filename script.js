995af3b8cc03085b251c63347a8f07f6bb95a7ab
markanjski <marko.adzic23@gmail.com>
2022-11-10 20:52:20 +0100

    Finished, probably

--- script.js
+++ script.js
@@ -17,27 +17,64 @@ function playerChoice() {
 function playRound() {
     let player = playerChoice();
     let comp = getComputerChoice();
-    console.log(player, comp);
-
-
+    console.log(`You chose ${player}, computer chose ${comp}`);
     playerNum = (player[0] == "R") ? 1 : ((player[0] == "P") ? 2 : 3);
     compNum = (comp[0] == "R") ? 1 : ((comp[0] == "P") ? 2 : 3);
     console.log(playerNum, compNum);
 
     if (playerNum - compNum == 0) {
-        console.log("Tie");
+        console.log("It's a Tie");
         return 0;
     } else if (playerNum - compNum == 1 || playerNum - compNum == -2) {
-        console.log("u won");
+        console.log("Congrats! You win");
         return 1;
-    } else console.log("comp won")
+    } else console.log("Too bad. Computer win");
     return 2;
 }
+
+function game() { //BEST OF FIVE
+    console.log("\n * * * NEW GAME * * *");
+    let scorePlayer = 0,
+        scoreComp = 0;
+    for (let i = 0; i < 5; i++) {
+        console.log(`--> round ${i+1}`);
+        let round = playRound();
+        switch (round) {
+            case 1:
+                scorePlayer++;
+                break;
+            case 2:
+                scoreComp++;
+                break;
+            default:
+                break;
+        }
+        console.log(`Current score: \nPlayer ${scorePlayer} : ${scoreComp} Computer\n\n`);
+    }
+    switch (true) {
+        case scorePlayer > scoreComp:
+            console.log(">>>>YOU HAVE WON!!!")
+            break;
+
+        case scoreComp > scorePlayer:
+            console.log(">>>>Computer have won!")
+            break;
+        case scoreComp == scorePlayer:
+            console.log(">>>>TIE!")
+            break;
+
+    }
+    console.log(scorePlayer, scoreComp);
+
+}
+
+game();
+
 
 
 
 
 
 for (let i = 0; i < 20; i++) {
-    console.table(getComputerChoice());
+
 }
